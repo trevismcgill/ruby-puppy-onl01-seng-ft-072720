42d3cddddb29e00ada0c842b3b612d4771f43626
Trevis McGill <trevismcgill25@gmail.com>
2020-08-06 01:55:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+require "pry"
+class Dog
+attr_accessor :name
+@@all = []
+
+def initialize(name)
+  #binding.pry
+@name = name
+save
+# binding.pry
+end
+
+def self.all
+@@all
+end
+
+def self.clear_all
+@@all = []
+end
+
+def self.print_all
+  @@all.each do |name|
+   puts name.name
+ end
+end
+def save
+  @@all.push(self)
+end
+end
